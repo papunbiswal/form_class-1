91781797fa7c7ac71efe55ba548e27ffb98af716
papunbiswal <biswalgyanaranjan2005@gmail.com>
2024-07-29 21:02:56 +0530

    add: form_class-1

---
+++ script.js
@@ -0,0 +1,20 @@
+function div1()
+{
+    document.getElementById("d1").style.backgroundColor = "red";
+}
+function div2()
+{
+    document.getElementById("d2").style.backgroundColor = "blue"
+}
+function div3()
+{
+    document.getElementById("d3").style.backgroundColor = "green"
+}
+function div4()
+{
+    document.getElementById("d4").style.backgroundColor = "yellow"
+}
+function div5()
+{
+    document.getElementById("d5").style.backgroundColor = "aqua"
+}
